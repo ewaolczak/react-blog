2d8e6720d124a9b7d1e7a7786d4e8fe83282ae01
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-18 23:48:36 +0100

    Add posts to state.posts

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,38 @@
 const initialState = {
-  posts: []
+  posts: [
+    {
+      id: '1',
+      title: 'First article title',
+      shortDescription: 'Short description of the first article...',
+      content: 'Main content of the first article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+    {
+      id: '2',
+      title: 'Second article title',
+      shortDescription: 'Short description of the second article...',
+      content: 'Main content of the second article',
+      publishedDate: '02-03-2022',
+      author: 'Amanda Clark'
+    },
+    {
+      id: '3',
+      title: 'Third article title',
+      shortDescription: 'Short description of the third article...',
+      content: 'Main content of the third article',
+      publishedDate: '02-04-2022',
+      author: 'Pablo Picasso'
+    },
+    {
+      id: '4',
+      title: 'Fourth article title',
+      shortDescription: 'Short description of the fourth article...',
+      content: 'Main content of the fourth article',
+      publishedDate: '02-02-2022',
+      author: 'Paris Hilton'
+    }
+  ]
 };
 
 export default initialState;
