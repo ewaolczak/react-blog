be8491f4d1a5e078968fdc10070edea273d5b5f5
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-20 01:02:03 +0100

    End adding post cards to Home and add links to buttons

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import PostsContainer from '../../func/PostsContainer/PostsContainer';
 
 const Home = () => {
   return (
     <>
-      <h1>All posts</h1>
+      <div className='d-sm-flex justify-content-sm-between'>
+        <h1>All posts</h1>
+        <Link to='/post/add'>
+          <Button variant='outline-info' className='m-2 me-0'>
+            Add post
+          </Button>
+        </Link>
+      </div>
       <PostsContainer />
     </>
   );
--- src/components/views/PostSingle/PostSingle.js
+++ src/components/views/PostSingle/PostSingle.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Card, Button, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const PostSingle = (props) => {
   return (
-    <Col xs='12' md='6' lg='4' className="mt-4">
+    <Col xs='12' md='6' lg='4' className='mt-4'>
       <Card className='mt=5'>
-        <Card.Body >
+        <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <Card.Subtitle className='fw-bold'>
+          <Card.Subtitle className='fw-bold mt-3'>
             Author: <span className='fw-normal'>{props.author}</span>
           </Card.Subtitle>
-          <Card.Subtitle className='fw-bold'>
-            Published:{' '}
-            <span className='fw-normal'>{props.publishedDate}</span>
+          <Card.Subtitle className='fw-bold mt-2'>
+            Published: <span className='fw-normal'>{props.publishedDate}</span>
           </Card.Subtitle>
-          <Card.Text>{props.shortDescription}</Card.Text>
-          <Button variant='primary'>Read more</Button>
+          <Card.Text className='mt-3'>{props.shortDescription}</Card.Text>
+          <Link to={`/post/${props.id}`}>
+          <Button className='mt-2' variant='primary'>
+            Read more
+          </Button>
+          </Link>
         </Card.Body>
       </Card>
     </Col>
