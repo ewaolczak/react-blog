d012b9a523a0f48fce3f3e9538a02440dc4fd28c
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-30 00:11:23 +0100

    Add react-datepicker package, Remove package-lock.json

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
-import ReactQuill from 'react-quill-2';
+import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const PostForm = ({ action, actionText, ...props }) => {
